79c9e443e2af798e1ba1780be22e3434f85fe64a
MVKarthikReddy <karthikmedagam@gmail.com>
2024-11-28 18:02:17 +0530

    minor changes

--- src/components/background/experienceset.jsx
+++ src/components/background/experienceset.jsx
@@ -1,5 +1,11 @@
 export const experience = [
     {
+        company_name : "Accenture",
+        role : "Application developer",
+        description : "",
+        duration : "Nov,2024 - present",
+    },
+    {
         company_name : "Provided by AWS Academy, AICTE",
         role : "AWS Cloud Intern",
         description : "Acquired a comprehensive understanding and expertise in numerous AWS services, such as  S3,  RDS, EC2, and several others. I have demonstrated my proficiency in implementing these services in AWS, delivering successful solutions that meet businesses unique requirements and objectives.",
